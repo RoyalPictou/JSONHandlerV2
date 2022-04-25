96c2b4e55055b702a6422485d1585ba81be03e68
Torben Schwarzer <torben.schwarzer@BK.local>
2022-04-25 14:56:50 +0200

    worked on parseEntry

--- JSONHandler/src/Handler.cpp
+++ JSONHandler/src/Handler.cpp
@@ -1,13 +1,13 @@
 #include "Handler.h"
-#include "../include/json.hpp"
+
 #include <iostream>
 
-using nlohmann::json;
+
 
 Handler::Handler()
 {
-std::string jsonData = "{ \"happy\": true, \"pi\" : 3.141 }";
-parseStringToJson(jsonData);
+    std::string jsonData = "{ \"happy\": true, \"pi\" : 3.141 }";
+    parseStringToJson(jsonData);
 
 }
 
@@ -15,9 +15,32 @@ Handler::~Handler()
 {
 }
 
+uint32_t Handler::createEntry(std::string name)
+{
+    uint32_t rv = 0;
+    json* set = new json();
+    if (set)
+    {
+        m_Entrys[name] = set;
+        rv = m_Entrys.size();
+    }
+    return rv;
+}
+
 void
 Handler::parseStringToJson(std::string& data)
 {
     json j3 = json::parse(data);
-std::cout << j3 << std::endl;
+    std::cout << j3 << std::endl;
+}
+
+json Handler::getJsonFromHexRep(std::vector<uint8_t> data)
+{
+    json rv = json::from_cbor(data);
+    return rv;
+}
+std::vector<uint8_t> Handler::getHexRep(json& data)
+{
+    std::vector<uint8_t> rv = json::to_cbor(data);
+return rv;
 }
--- JSONHandler/src/Handler.h
+++ JSONHandler/src/Handler.h
@@ -1,14 +1,26 @@
 #ifndef HANDLER__H_
 #define HANDLER__H_
+
+#include "../include/json.hpp"
 #include <string>
+
+#include <vector>
+#include <unordered_map>
+
+using nlohmann::json;
 class Handler
 {
 public:
     Handler();
     ~Handler();
-    void parseStringToJson(std::string& data);
-private:
+    uint32_t createEntry(std::string name);
 
+    void parseStringToJson(std::string& data);
+    json getJsonFromHexRep(std::vector<uint8_t> data);
+    std::vector<uint8_t> getHexRep(json& data);
+private:
+ std::unordered_map<std::string,json*> m_Entrys;
+    
 
 };
 
