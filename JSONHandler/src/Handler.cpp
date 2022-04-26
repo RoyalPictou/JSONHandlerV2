70dfc8875f02750cb270eb1029479db4c2b19abe
Torben Schwarzer <torben.schwarzer@BK.local>
2022-04-26 14:44:38 +0200

    Added some functions to create Entry elements

--- JSONHandler/src/Handler.cpp
+++ JSONHandler/src/Handler.cpp
@@ -18,14 +18,66 @@ Handler::~Handler()
 uint32_t Handler::createEntry(std::string name)
 {
     uint32_t rv = 0;
-    json* set = new json();
-    if (set)
-    {
-        m_Entrys[name] = set;
-        rv = m_Entrys.size();
-    }
+    m_EntryName = name;
+    //m_Data = new json();
+   // if (m_Data)
+   // {
+        //m_Entrys[name] = set;
+    //    rv = m_Entrys.size();
+   // }
     return rv;
 }
+uint32_t Handler::addEntry(json & data, std::string& key, std::string& value)
+{
+    uint32_t rv = 0;
+    data[key] = value;
+    return rv;
+}
+uint32_t Handler::removeEntry(json& data, std::string key)
+{
+    uint32_t rv = data.erase(key);
+    return rv;
+}
+void Handler::addArrayEntry(json& data, std::vector<uint32_t> newarray)
+{
+    data.append(newarray);
+
+}
+std::vector<int> c_vector{ 1, 2, 3, 4 };
+
+// [1, 2, 3, 4]
+
+std::deque<double> c_deque{ 1.2, 2.3, 3.4, 5.6 };
+json j_deque(c_deque);
+// [1.2, 2.3, 3.4, 5.6]
+
+std::list<bool> c_list{ true, true, false, true };
+json j_list(c_list);
+// [true, true, false, true]
+
+std::forward_list<int64_t> c_flist{ 12345678909876, 23456789098765, 34567890987654, 45678909876543 };
+json j_flist(c_flist);
+// [12345678909876, 23456789098765, 34567890987654, 45678909876543]
+
+std::array<unsigned long, 4> c_array{ {1, 2, 3, 4} };
+json j_array(c_array);
+// [1, 2, 3, 4]
+
+std::set<std::string> c_set{ "one", "two", "three", "four", "one" };
+json j_set(c_set); // only one entry for "one" is used
+// ["four", "one", "three", "two"]
+
+std::unordered_set<std::string> c_uset{ "one", "two", "three", "four", "one" };
+json j_uset(c_uset); // only one entry for "one" is used
+// maybe ["two", "three", "four", "one"]
+
+std::multiset<std::string> c_mset{ "one", "two", "one", "four" };
+json j_mset(c_mset); // both entries for "one" are used
+// maybe ["one", "two", "one", "four"]
+
+std::unordered_multiset<std::string> c_umset{ "one", "two", "one", "four" };
+json j_umset(c_umset); // both entries for "one" are used
+// maybe ["one", "two", "one", "four"]
 
 void
 Handler::parseStringToJson(std::string& data)
@@ -42,5 +94,5 @@ json Handler::getJsonFromHexRep(std::vector<uint8_t> data)
 std::vector<uint8_t> Handler::getHexRep(json& data)
 {
     std::vector<uint8_t> rv = json::to_cbor(data);
-return rv;
+    return rv;
 }
--- JSONHandler/src/Handler.h
+++ JSONHandler/src/Handler.h
@@ -15,12 +15,19 @@ public:
     ~Handler();
     uint32_t createEntry(std::string name);
 
+    uint32_t addEntry(json& data, std::string& key, std::string& value);
+
+    uint32_t removeEntry(json& data, std::string key);
+
+    void addArrayEntry(json& data, std::vector<uint32_t>);
+
     void parseStringToJson(std::string& data);
     json getJsonFromHexRep(std::vector<uint8_t> data);
     std::vector<uint8_t> getHexRep(json& data);
 private:
  std::unordered_map<std::string,json*> m_Entrys;
-    
+    std::string m_EntryName;
+json m_Data;
 
 };
 
